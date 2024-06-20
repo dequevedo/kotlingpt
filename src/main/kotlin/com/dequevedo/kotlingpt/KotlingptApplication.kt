a31df64c07b7d42c46496c1d403cda91a06ec469
dequevedo <danielelvisquevedo@gmail.com>
2024-06-19 21:11:58 -0300

    Initial commit

---
+++ src/main/kotlin/com/dequevedo/kotlingpt/KotlingptApplication.kt
@@ -0,0 +1,11 @@
+package com.dequevedo.kotlingpt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlingptApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlingptApplication>(*args)
+}
---
+++ src/test/kotlin/com/dequevedo/kotlingpt/KotlingptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dequevedo.kotlingpt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlingptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
